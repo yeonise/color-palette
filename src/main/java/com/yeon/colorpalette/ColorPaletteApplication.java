8778d5a0cfd27d88038128572cb1bad476aafe22
yeonise <cerealnum@gmail.com>
2023-10-08 12:23:55 +0900

    chore: Initialization

---
+++ src/main/java/com/yeon/colorpalette/ColorPaletteApplication.java
@@ -0,0 +1,13 @@
+package com.yeon.colorpalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorPaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorPaletteApplication.class, args);
+	}
+
+}
