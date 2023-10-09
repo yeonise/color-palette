e70b81a4fe69b1262b2021cd974cb3093cb7767b
yeonise <cerealnum@gmail.com>
2023-10-09 15:27:19 +0900

    feat: [domain] Member Entity 생성
    Enum Provider, Enum Role 또한 생성합니다.

---
+++ src/main/java/com/yeon/colorpalette/member/domain/Member.java
@@ -0,0 +1,35 @@
+package com.yeon.colorpalette.member.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String email;
+	private String nickname;
+	private String password;
+
+	@Enumerated(EnumType.STRING)
+	private Provider provider;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	private boolean isDeleted;
+
+}
