128322db040c7ef7946258d1f97264c89b22c634
yeonise <cerealnum@gmail.com>
2023-10-15 20:36:35 +0900

    feat: [config] Interceptor를 통과할 path 경로 추가
    OAuth 인증을 위한 endpoint를 제외합니다.

--- src/main/java/com/yeon/colorpalette/config/WebConfig.java
+++ src/main/java/com/yeon/colorpalette/config/WebConfig.java
@@ -35,7 +35,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(authInterceptor)
 			.addPathPatterns("/**")
-			.excludePathPatterns("/api/oauth/{provider}");
+			.excludePathPatterns("/api/oauth");
 	}
 
 	@Override
