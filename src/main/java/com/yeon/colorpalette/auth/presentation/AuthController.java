86af033a7c2037e3dcdba25a50cbcdec0dd1310e
yeonise <cerealnum@gmail.com>
2023-10-15 20:31:45 +0900

    feat: [presentation] OAuth 로그인을 위한 endpoint 생성

--- src/main/java/com/yeon/colorpalette/auth/presentation/AuthController.java
+++ src/main/java/com/yeon/colorpalette/auth/presentation/AuthController.java
@@ -3,7 +3,6 @@ package com.yeon.colorpalette.auth.presentation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.yeon.colorpalette.api.ApiResponse;
 import com.yeon.colorpalette.api.ResponseMessage;
 import com.yeon.colorpalette.auth.application.AuthService;
+import com.yeon.colorpalette.auth.application.response.AccessTokenResponse;
+import com.yeon.colorpalette.auth.application.response.LoginResponse;
 import com.yeon.colorpalette.auth.domain.Account;
 import com.yeon.colorpalette.auth.presentation.request.LoginRequest;
 import com.yeon.colorpalette.config.resolver.Login;
-import com.yeon.colorpalette.auth.application.response.AccessTokenResponse;
-import com.yeon.colorpalette.auth.application.response.LoginResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -44,9 +43,9 @@ public class AuthController {
 		return ApiResponse.ok(ResponseMessage.REISSUE_ACCESS_TOKEN_SUCCESS.getMessage(), authService.reissueAccessToken(account));
 	}
 
-	@GetMapping("/oauth/{provider}")
-	public ApiResponse<AccessTokenResponse> loginByOAuth(@PathVariable String provider, @RequestParam String code) {
-		return ApiResponse.ok(ResponseMessage.LOGIN_SUCCESS.getMessage(), authService.loginByOAuth(provider, code));
+	@GetMapping("/oauth")
+	public ApiResponse<LoginResponse> loginByOAuth(@RequestParam String code) {
+		return ApiResponse.ok(ResponseMessage.LOGIN_SUCCESS.getMessage(), authService.loginByOAuth(code));
 	}
 
 }
