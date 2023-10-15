6d15b8d7da392c149edabb6472a9968161247eb2
yeonise <cerealnum@gmail.com>
2023-10-15 20:44:03 +0900

    feat: [infrastructure] OAuth 로그인을 위한 인터페이스 생성

--- src/main/java/com/yeon/colorpalette/member/infrastructure/MemberRepository.java
+++ src/main/java/com/yeon/colorpalette/member/infrastructure/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import com.yeon.colorpalette.auth.domain.Provider;
 import com.yeon.colorpalette.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -13,6 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
 	boolean existsByNickname(String nickname);
 	Optional<Member> findByEmailAndPasswordAndIsDeleted(String email, String password, boolean isDeleted);
+	Optional<Member> findByEmailAndProviderAndIsDeleted(String email, Provider provider, boolean isDeleted);
 
 	@Modifying
 	@Query(value = "UPDATE Member m SET m.isDeleted = true WHERE id = :id ")
