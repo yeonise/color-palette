ae9b6f92e21645b2c4475ab5804a115b325bbc6c
yeonise <cerealnum@gmail.com>
2023-10-22 21:15:42 +0900

    feat: [domain] Paltte에 Bookmark와의 관계 추가

--- src/main/java/com/yeon/colorpalette/palette/domain/Palette.java
+++ src/main/java/com/yeon/colorpalette/palette/domain/Palette.java
@@ -1,17 +1,22 @@
 package com.yeon.colorpalette.palette.domain;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.CreatedDate;
 
+import com.yeon.colorpalette.member.domain.Bookmark;
 import com.yeon.colorpalette.member.domain.Member;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -41,9 +46,20 @@ public class Palette {
 
 	private int views;
 
+	@OneToMany(mappedBy = "palette", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Bookmark> bookmarks = new ArrayList<>();
+
 	@CreatedDate
 	private LocalDateTime createdAt;
 
 	private boolean isDeleted;
+
+	public String findCreator() {
+		return member.getNickname();
+	}
+
+	public String findTagName() {
+		return tag == null ? null : tag.getName();
+	}
 
 }
